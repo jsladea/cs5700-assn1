2ed1f43ffed4f3caf5efd7ab2ffed90b1cbf73f0
jsladea <jaaronslade@gmail.com>
2025-07-01 20:10:19 -0600

    Added second constructor to Line

--- assn1/src/main/kotlin/shapes/Line.kt
+++ assn1/src/main/kotlin/shapes/Line.kt
@@ -9,6 +9,8 @@ class Line(private val p1: Point, private val p2: Point): IMoveable, ICloneable<
         require(p1.x != p2.x || p1.y != p2.y) { "Line cannot have length of zero" }
     }
 
+    constructor(x1: Double, y1: Double, x2: Double, y2: Double) : this(Point(x1, y1), Point(x2, y2))
+
     fun getP1(): Point = p1.clone()
     fun getP2(): Point = p2.clone()
 
