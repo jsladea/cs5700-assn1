3b505d6ffe305977542f17f82ddc7c82d15c712e
jsladea <jaaronslade@gmail.com>
2025-07-01 19:52:46 -0600

    Added Interfaces

---
+++ assn1/src/main/kotlin/shapes/interfaces/ICloneable.kt
@@ -0,0 +1,5 @@
+package shapes.interfaces
+
+interface ICloneable<T> {
+    fun clone(): T
+}
---
+++ assn1/src/main/kotlin/shapes/interfaces/IMoveable.kt
@@ -0,0 +1,5 @@
+package shapes.interfaces
+
+interface IMoveable {
+    fun move(dx: Double, dy: Double)
+}
